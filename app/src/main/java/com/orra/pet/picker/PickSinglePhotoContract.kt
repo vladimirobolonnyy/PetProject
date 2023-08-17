7559f8844f9fce893c9576d0b35b8dabc01ade39
vladimir <vladimir@gmail.com>
2023-08-17 12:27:52 +0300

    refactoring

--- app/src/main/java/com/orra/pet/picker/PickSinglePhotoContract.kt
+++ app/src/main/java/com/orra/pet/picker/PickSinglePhotoContract.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
+import android.os.ext.SdkExtensions
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContract
 
@@ -36,5 +38,20 @@ class PickSinglePhotoContract : ActivityResultContract<Void?, Uri?>() {
 
     override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
         return intent.takeIf { resultCode == Activity.RESULT_OK }?.data
+    }
+}
+
+object PhotoPickerAvailabilityChecker {
+    private const val ANDROID_R_REQUIRED_EXTENSION_VERSION = 2
+
+    fun isPhotoPickerAvailable(): Boolean {
+        return when {
+            Build.VERSION.SDK_INT >= 33 -> true
+            Build.VERSION.SDK_INT >= 30 -> {
+                SdkExtensions.getExtensionVersion(Build.VERSION_CODES.R) >= ANDROID_R_REQUIRED_EXTENSION_VERSION
+            }
+
+            else -> false
+        }
     }
 }
